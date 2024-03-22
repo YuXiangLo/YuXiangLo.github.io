32a075c0004d0ea58b136ff66ea0bf93a89b46a0
YuXiangLo <yuxiang@yuxiang.local>
2024-03-22 14:11:36 +0800

    react app

---
+++ cv-site/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHammer, faFolder } from '@fortawesome/free-solid-svg-icons';
+import './App.css'; // Make sure your CSS file is correctly referenced
+
+function App() {
+  // Replace "https://example.com/your-cv" with your actual URL
+  const URL = "https://github.com/YuXiangLo/YuXiangLo.github.io";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FontAwesomeIcon icon={faHammer} className="App-logo" spin />
+        <p>
+          Our website is currently under construction.
+        </p>
+        <p>
+          Check back soon!
+        </p>
+        {/* Folder Icon that links to your CV */}
+        <a href={URL} target="_blank" rel="noopener noreferrer" className="App-link">
+          <FontAwesomeIcon icon={faFolder} /> My CV
+
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
