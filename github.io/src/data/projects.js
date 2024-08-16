8018ea218b60d2723353a81c9eeac4c3907bf1c1
YuXiangLo <linus13514@gmail.com>
2024-08-16 16:24:16 +0800

    force download hahaha

--- github.io/src/data/projects.js
+++ github.io/src/data/projects.js
@@ -3,7 +3,7 @@ const data = [
   {
     title: 'GenAI in Web Services',
     subtitle: 'Paper: Extracting Tuple-Based Service Demands with Large Language Models for Automated Service Composition',
-    link: 'https://github.com/YuXiangLo/YuXiangLo.github.io/blob/main/CV_folder/not_sure_but_probably_ICWS_paper.pdf',
+    link: 'https://raw.githubusercontent.com/YuXiangLo/YuXiangLo.github.io/main/CV_folder/not_sure_but_probably_ICWS_paper.pdf',
     image: '/images/projects/ICWS_paper.png',
     date: '2024-04-02',
     desc:
