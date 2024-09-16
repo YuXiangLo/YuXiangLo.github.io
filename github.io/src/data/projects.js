643c8a58e17610c8823e21ea86ec05fdb4293029
YuXiangLo <linus13514@gmail.com>
2024-09-16 13:20:10 +0800

    update website

--- github.io/src/data/projects.js
+++ github.io/src/data/projects.js
@@ -1,9 +1,9 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'GenAI in Web Services',
+    title: 'SCC 2024 Paper',
     subtitle: 'Paper: Extracting Tuple-Based Service Demands with Large Language Models for Automated Service Composition',
-    link: 'https://raw.githubusercontent.com/YuXiangLo/YuXiangLo.github.io/main/CV_folder/not_sure_but_probably_ICWS_paper.pdf',
+    link: 'https://raw.githubusercontent.com/YuXiangLo/YuXiangLo.github.io/main/CV_folder/SCC_2024_Paper.pdf',
     image: '/images/projects/ICWS_paper.png',
     date: '2024-04-02',
     desc:
