fbd91b3bbcc70d581b04c0985b93b7c2d9a1ca73
YuXiangLo <linus13514@gmail.com>
2024-12-13 13:33:50 +0800

    update: update game page and game

--- github.io/src/pages/Game.js
+++ github.io/src/pages/Game.js
@@ -13,21 +13,30 @@ const Projects = () => (
           <h2><Link to="/games">Game</Link></h2>
         </div>
       </header>
-      <p>
+      <h>
         This is the project I&apos;m working now.
         It&apos;s just a prototype but I still want to share this with you!
-      </p>
+      </h>
+      <br />
+      <h>
+        I suggest to play the game in full screen for better experience!
+      </h>
+      <br />
+      <br />
       <ul>
-        <li> Press &apos;Space&apos; to jump.</li>
+        <li> Press &apos;E&apos; to view bulletin</li>
         <li> Press &apos;AD&apos; to move.</li>
-        <li> Press &apos;E&apos; to enter a door.</li>
-        <li> Press &apos;QE&apos; to get hurt or heal.</li>
+        <li> Press &apos;Space&apos; to jump.</li>
+        <li> Hold &apos;Space&apos; to float.</li>
+        <li> Press &apos;W&apos; to climb</li>
+        <li> Press &apos;F&apos; to fire</li>
+        <li> Hold &apos;F&apos; to charge</li>
       </ul>
       <iframe
-        title="Mario Demo"
+        title="Bubblo Demo"
         src="/webgl/index.html"
         width="100%"
-        height="800px"
+        height="540px"
         style={{ border: 'none' }}
         allowFullScreen
       />
